199826a20fc3dc82e2942485d6a618e78d1d9cfe
Ward <Jeff.Ward@umusic.com>
2016-07-19 22:20:45 -0700

    working v1

--- websiteMonitor/websiteMonitor.Console/Program.cs
+++ websiteMonitor/websiteMonitor.Console/Program.cs
@@ -1,8 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Xml;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.PhantomJS;
+using websiteMonitor.Console.Services;
+using HtmlDocument = System.Windows.Forms.HtmlDocument;
+using Timer = System.Timers.Timer;
 
 namespace websiteMonitor.Console
 {
@@ -11,9 +25,76 @@ namespace websiteMonitor.Console
         static void Main(string[] args)
         {
 
-            //http://stackoverflow.com/questions/1189364/reading-settings-from-app-config-or-web-config-in-net
+            // Create a timer
+            var myTimer = new System.Timers.Timer();
+            // Tell the timer what to do when it elapses
+            myTimer.Elapsed += new ElapsedEventHandler(ringMonitor);
+            // Set it to go off every five seconds
+            myTimer.Interval = 5000;
+            // And start it        
+            myTimer.Enabled = true;
 
 
+
+
+            System.Console.ReadLine();
+
+
+        }
+
+
+
+
+
+
+
+
+
+        // Implement a call with the right signature for events going off
+        private static void ringMonitor(object source, ElapsedEventArgs e)
+        {
+
+
+            var service = new websiteService();
+            IWebDriver driver = new PhantomJSDriver();
+            driver.Navigate().GoToUrl("https://betterthandiamond.com/amora-gem-search.php");
+          //driver.Navigate().GoToUrl("http://weddingmonitor.jeffwarddevelopment.com/");
+            try
+            {
+                var exists = driver.FindElement(By.ClassName("notificationlink")); 
+                var exists2 = driver.FindElement(By.ClassName("notFound"));
+                {
+                    System.Console.Clear();
+                    var key = "(We are sold out! Please click here to be notified when we restock...)";
+                    var key2 = "NO RESULTS";
+                    if (exists.Text == key && exists2.Text == key2)
+                    {
+                        System.Console.WriteLine("This is Here! MATCH!");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("NOT FOUND!");
+                        service.sendSMS();
+
+                    }
+                    driver.Close();
+                    System.Console.ReadLine();
+                }
+            }
+            catch (Exception)
+            {
+
+                System.Console.WriteLine("Not found");
+                driver.Close();
+
+                System.Console.ReadLine();
+            }
+
+
+
+            driver.Close();
+
+            System.Console.ReadLine();
         }
     }
 }
---
+++ websiteMonitor/websiteMonitor.Console/Services/websiteService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+
+namespace websiteMonitor.Console.Services
+{
+    public class websiteService
+    {
+        public bool WatchWeddingData(string website)
+        {
+
+            return true;
+        }
+
+
+        public bool sendSMS()
+        {
+            // set our AccountSid and AuthToken
+            string AccountSid = "AC61b76d7cf5033d39d3fdf1a6816e3e61";
+            string AuthToken = "1f4545334cf64e12d68a224de622178c";
+
+
+
+            var options = new CallOptions();
+            options.Url = "http://demo.twilio.com/docs/voice.xml";
+            options.To = "+19492593445";
+            options.From = "+16176740897";
+            // instantiate a new Twilio Rest Client
+            var client = new TwilioRestClient(AccountSid, AuthToken);
+            try
+            {
+               // var call = client.InitiateOutboundCall(options);
+                client.SendMessage(options.From, options.To, "TEST");
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
