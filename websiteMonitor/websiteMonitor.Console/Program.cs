636f645987efd1265f5cca70311e2e6818523c3a
jeffward01 <jeffwardcoding@gmail.com>
2016-07-22 22:19:37 -0700

    memory leak tweak

--- websiteMonitor/websiteMonitor.Console/Program.cs
+++ websiteMonitor/websiteMonitor.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing.Text;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -22,26 +23,52 @@ namespace websiteMonitor.Console
 {
     class Program
     {
+        public interface IProcessor
+        {
+            void Run();
+        }
+
+        public interface IWorker
+        {
+            void DoWork();
+        }
+
+        public interface ILogger
+        {
+            void Log(string logDetails, int level);
+        }
+
         static void Main(string[] args)
         {
-            
+            //Window Size
+
+
+            System.Console.WriteLine("System Starting...");
             // Create a timer
             var myTimer = new System.Timers.Timer();
             // Tell the timer what to do when it elapses
             myTimer.Elapsed += new ElapsedEventHandler(ringMonitor);
+
             // Set it to go off every 60 seconds
             myTimer.Interval = 60000;
             // And start it        
             myTimer.Enabled = true;
-           
-            
+            myTimer.AutoReset = true;
 
-            
 
+
+
+
+
+            // Code here will not he hit, due to Processor class' readline.
             System.Console.ReadLine();
-
-
         }
+
+
+
+
+
+
 
 
 
@@ -55,49 +82,50 @@ namespace websiteMonitor.Console
         private static void ringMonitor(object source, ElapsedEventArgs e)
         {
             var service = new websiteService();
-            IWebDriver driver = new PhantomJSDriver();
-            driver.Navigate().GoToUrl("https://betterthandiamond.com/amora-gem-search.php");
-            //driver.Navigate().GoToUrl("http://weddingmonitor.jeffwarddevelopment.com/");
-            try
+            using (var driver = new PhantomJSDriver())
             {
-                var exists = driver.FindElement(By.ClassName("notificationlink"));
-                var exists2 = driver.FindElement(By.ClassName("notFound"));
+                driver.Navigate().GoToUrl("https://betterthandiamond.com/amora-gem-search.php");
+                //driver.Navigate().GoToUrl("http://weddingmonitor.jeffwarddevelopment.com/");
+                try
                 {
-                    System.Console.Clear();
-                    var key = "(We are sold out! Please click here to be notified when we restock...)";
-                    var key2 = "NO RESULTS";
-                    if (exists.Text == key && exists2.Text == key2)
+                    var exists = driver.FindElement(By.ClassName("notificationlink"));
+                    var exists2 = driver.FindElement(By.ClassName("notFound"));
                     {
                         System.Console.Clear();
-                        System.Console.WriteLine("Matching... Ring out of stock");
-                        
+                        var key = "(We are sold out! Please click here to be notified when we restock...)";
+                        var key2 = "NO RESULTS";
+                        if (exists.Text == key && exists2.Text == key2)
+                        {
+                            System.Console.Clear();
+                            System.Console.WriteLine("Matching... Ring out of stock");
 
+
+                        }
+                        else
+                        {
+                            System.Console.Clear();
+                            service.sendSMS();
+                            System.Console.WriteLine("NOT FOUND! Ring in stock!");
+
+                            Environment.Exit(0);
+                        }
+                        driver.Close();
+                        System.Console.ReadLine();
                     }
-                    else
-                    {
-                        System.Console.Clear();
-                        service.sendSMS();
-                        System.Console.WriteLine("NOT FOUND! Ring in stock!");
-                        
-                        Environment.Exit(0);
-                    }
+                }
+                catch (Exception)
+                {
+
+                    System.Console.WriteLine("Not found");
                     driver.Close();
+
                     System.Console.ReadLine();
                 }
-            }
-            catch (Exception)
-            {
 
-                System.Console.WriteLine("Not found");
+
+
                 driver.Close();
-
-                System.Console.ReadLine();
             }
-
-
-
-            driver.Close();
-
             System.Console.ReadLine();
         }
     }
